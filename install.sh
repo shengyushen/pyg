ecbfb41cdf010797ebc53aa35bfbd621796a4add
shengyushen <shengyushen@aliyun.com>
2020-03-25 06:40:09 -0400

    adding files

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate https://s3.eu-central-1.amazonaws.com/pytorch-geometric.com/whl/torch-1.4.0/torch_scatter-latest+cu101-cp37-cp37m-linux_x86_64.whl
+wget --no-check-certificate https://s3.eu-central-1.amazonaws.com/pytorch-geometric.com/whl/torch-1.4.0/torch_sparse-latest+cu101-cp37-cp37m-linux_x86_64.whl
+wget --no-check-certificate https://s3.eu-central-1.amazonaws.com/pytorch-geometric.com/whl/torch-1.4.0/torch_cluster-latest+cu101-cp37-cp37m-linux_x86_64.whl
+wget --no-check-certificate https://s3.eu-central-1.amazonaws.com/pytorch-geometric.com/whl/torch-1.4.0/torch_spline-conv-latest+cu101-cp37-cp37m-linux_x86_64.whl
